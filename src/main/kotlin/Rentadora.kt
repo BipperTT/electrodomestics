adb25765dc2fb37ecdf5520ac98d9f53617d0c24
Alexis <alexis.checa.7e7@itb.cat>
2024-02-04 17:31:15 +0100

    4to Commit: primera versión de la clase Televisio

--- src/main/kotlin/Rentadora.kt
+++ src/main/kotlin/Rentadora.kt
@@ -3,7 +3,7 @@ package org.example
 class Rentadora(var c_arrega: Int = 5) : Electrodomestic() {
     override fun preu_final(): Double {
         var preu_final = super.preu_final()
-        
+
         when (c_arrega) {
             6, 7 -> preu_final += 55
             8 -> preu_final += 70
--- src/main/kotlin/Televisio.kt
+++ src/main/kotlin/Televisio.kt
@@ -1,4 +1,16 @@
 package org.example
 
-class Televisio {
+class Televisio(var mida: Int = 28) : Electrodomestic() {
+    override fun preu_final(): Double {
+        var preu_final = super.preu_final()
+        
+        when {
+            mida in 29..32 -> preu_final += 50
+            mida <= 42 -> preu_final += 100
+            mida <= 50 -> preu_final += 150
+            else -> preu_final += 200
+        }
+
+        return preu_final
+    }
 }
