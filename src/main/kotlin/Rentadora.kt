e734265d2f2d070ca07789d591abdf6f1f50452c
Alexis <alexis.checa.7e7@itb.cat>
2024-02-04 17:21:07 +0100

    3er Commit: primera versión de la clase Rentadora

--- src/main/kotlin/Rentadora.kt
+++ src/main/kotlin/Rentadora.kt
@@ -1,4 +1,16 @@
 package org.example
 
-class Rentadora {
+class Rentadora(var c_arrega: Int = 5) : Electrodomestic() {
+    override fun preu_final(): Double {
+        var preu_final = super.preu_final()
+        
+        when (c_arrega) {
+            6, 7 -> preu_final += 55
+            8 -> preu_final += 70
+            9 -> preu_final += 85
+            10 -> preu_final += 100
+        }
+
+        return preu_final
+    }
 }
